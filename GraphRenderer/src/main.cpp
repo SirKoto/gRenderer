76513d87fa489057720544db02d5a8435dbe87e2
Pol Martín Garcia <pol147258369@gmail.com>
2020-11-23 11:55:53 +0100

    New jobified main

--- GraphRenderer/src/main.cpp
+++ GraphRenderer/src/main.cpp
@@ -1,21 +1,32 @@
 
 #include "Engine.h"
-#include <exception>
-#include <iostream>
+#include "utils/grjob.h"
 
 int main() {
 
 	gr::Engine::init();
 
-	gr::Engine engine;
-	try {
+	gr::grjob::createSystem(6);
+
+	gr::grjob::Job mainJob([]() {
+
+		gr::Engine engine;
+
 		engine.run();
-	}
-	catch (const std::exception& exc) {
-		std::cerr << exc.what() << std::endl;
-		return 1;
-	}
+		
+		gr::grjob::stopRunningJobSystem();
+	});
+
+
+
+	gr::grjob::runJobOnMainThread(mainJob, nullptr, true);
+
+	gr::grjob::startRunningJobSystem();
+
 
 	gr::Engine::terminate();
+
+	gr::grjob::destroySystem();
+
 	return 0;
 }

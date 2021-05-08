c36d46f66073a35ccd5900a6f8ca8af275858a8f
SirKoto <pol147258369@gmail.com>
2021-05-08 18:11:59 +0200

    Quadric error metric simplification

--- GraphRenderer/src/meshes/Mesh/LODGeneration.cpp
+++ GraphRenderer/src/meshes/Mesh/LODGeneration.cpp
@@ -3,13 +3,14 @@
 
 #include "../Mesh.h"
 
-
+#include <Eigen/Dense>
+#include <Eigen/src/SVD/JacobiSVD.h>
 #include <bitset>
 
-struct Node
-{
-	std::bitset<8> nodeTypes{ 0 };
-};
+
+typedef Eigen::Matrix4d Mat4;
+typedef Eigen::Vector4d Vec4;
+
 
 struct Task {
 	std::vector<uint32_t> vertices;
@@ -58,6 +59,39 @@ void gr::Mesh::regenerateLODs(FrameContext* fc)
 	for (auto& v : old2newVerticesInLod) v.resize(mVertices.size());
 
 
+	// Compute the planes of all triangles
+	std::vector<Vec4> trianglePlanes(mIndices.size() / 3);
+	for (uint32_t t = 0; t < (uint32_t)mIndices.size() / 3; ++t) {
+		const glm::vec3& v0 = mVertices[mIndices[3 * t + 0]].pos;
+		const glm::vec3& v1 = mVertices[mIndices[3 * t + 1]].pos;
+		const glm::vec3& v2 = mVertices[mIndices[3 * t + 2]].pos;
+
+		glm::vec3 n = glm::cross(v1 - v0, v2 - v0);
+		n = glm::normalize(n);
+
+		float d = -glm::dot(n, v0);
+
+		trianglePlanes[t] = Vec4(n.x, n.y, n.z, d);
+	}
+
+	// Compute V:{F}
+	// Vertex positions on the structure.
+	std::vector<std::vector<uint32_t>> vert2faces(mVertices.size());
+	std::vector<uint32_t> vertexArity(mVertices.size(), 0);
+	for (uint32_t t = 0; t < (uint32_t)mIndices.size() / 3; ++t) {
+		vertexArity[mIndices[3 * t + 0]] += 1;
+		vertexArity[mIndices[3 * t + 1]] += 1;
+		vertexArity[mIndices[3 * t + 2]] += 1;
+	}
+	for (uint32_t v = 0; v < (uint32_t)mVertices.size(); ++v) {
+		vert2faces[v].reserve(vertexArity[v]);
+	}
+	for (uint32_t t = 0; t < (uint32_t)mIndices.size() / 3; ++t) {
+		vert2faces[mIndices[3 * t + 0]].push_back(t);
+		vert2faces[mIndices[3 * t + 1]].push_back(t);
+		vert2faces[mIndices[3 * t + 2]].push_back(t);
+	}
+
 	while (!tasks.empty()) {
 
 		const Task task = std::move(tasks.top());
@@ -78,10 +112,8 @@ void gr::Mesh::regenerateLODs(FrameContext* fc)
 
 		// keep generating nodes....
 		if (maxDepth  > task.depth) {
-			Node node;
 
 			for (uint32_t k = 0; k < 8; ++k) {
-				node.nodeTypes[k] = !childsVert[k].empty();
 
 				if (!childsVert[k].empty()) {
 					glm::vec3 dir = { k & 0b1 ? 1 : -1, k & 0b10 ? 1 : -1, k & 0b100 ? 1 : -1 };
@@ -92,8 +124,6 @@ void gr::Mesh::regenerateLODs(FrameContext* fc)
 					tasks.push(std::move(newT));
 				}
 			}
-
-			// nodes.push_back(node);
 		}
 
 		// if this depth is one of the LODs, store vertex
@@ -101,10 +131,40 @@ void gr::Mesh::regenerateLODs(FrameContext* fc)
 		if (it != depthToLod.end()) {
 			Vertex v;
 			v.pos = task.midCoord;
-			v.normal = mVertices[task.vertices.front()].normal;
+			v.normal = glm::vec3(0);
+
+			// Quadric error metric
+			{
+				Mat4 K = Mat4::Zero();
+
+				for (const uint32_t& vId : task.vertices) {
+					for (const uint32_t& f : vert2faces[vId]) {
+						const Vec4& P = trianglePlanes[f];
+
+						const Vec4 p = Vec4(P[0], P[1], P[2], P[3] + P.dot(Vec4(task.midCoord.x, task.midCoord.y, task.midCoord.z, 0)));
+						K += p * p.transpose();
+					}
+					v.normal += mVertices[vId].normal;
+				}
+				v.normal /= task.vertices.size();
+
+				const Vec4 rhs = Vec4(0, 0, 0, 1.0);
+				// Set last row to 0, 0, 0, 1
+				K.row(3) = rhs;
+				// Compute SVD
+				Eigen::JacobiSVD<Mat4> svd(K, Eigen::ComputeFullU | Eigen::ComputeFullV);
+				svd.setThreshold(1e-6);
+				const Vec4 res = svd.solve(rhs);
+
+				v.pos = glm::vec3(res[0], res[1], res[2]) + task.midCoord;
+			}
+
+
+			// store new vertex
 			uint32_t vertexIdx = (uint32_t) mLODs[it->second].vertices.size();
 			mLODs[it->second].vertices.push_back(v);
 
+			// Store translation table of the new vertex
 			for (uint32_t i : task.vertices) {
 				old2newVerticesInLod[it->second][i] = vertexIdx;
 			}
